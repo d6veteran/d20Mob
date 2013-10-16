dcace7f33525c242b80e3f0e627580fa722c1c8b
Will Merydith <willmery@microsoft.com>
2013-10-16 14:30:31 -0700

    Express install

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!  Welcome to Node.js running on Azure.');
